d8232845348f84ec2b55c8e1579777eed0477c4d
Antoine <antoine.couty@etu.unilim.fr>
2022-02-16 14:17:56 +0100

    render parallélisé

--- src/scene.cpp
+++ src/scene.cpp
@@ -38,7 +38,7 @@ namespace RT_ISICG
 		_attachMaterialToObject( "Blue", "Sphere1" );
 		_attachMaterialToObject( "Red", "Plane1" );
 
-		_addLight( new PointLight( WHITE, 100.f, Vec3f(1.f, 10.f, 1.f) ) );
+		_addLight( new PointLight( WHITE, 100.f, Vec3f(4.f, 10.f, 1.f) ) );
 	}
 
 	bool Scene::intersect( const Ray & p_ray, const float p_tMin, const float p_tMax, HitRecord & p_hitRecord ) const
